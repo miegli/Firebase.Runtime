a0634aec7c8495ca5af3aaba4311b55b3c534118
miegli <michae.egli@phlu.ch>
2017-10-10 14:48:11 +0200

    added basic functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,76 @@
-const functions = require('firebase-functions');
+/**
+ * Copyright (c) 2017 by Michael Egli
+ *
+ *
+ *
+ * firebase database structure
+ *
+ * - user
+ * --- {userid}
+ * ----- storage
+ * ------ {object} business objects
+ * ------- {objectid} business object identifier / single record
+ * --------- version (mixed, timestamp|uui|identifier) change it to fetch current content data from api
+ * --------- data (mixed)
+ * ---------- bind (mixed) json data of current version (is three way binding to client)
+ * ---------- stable (mixed) json data of current version (is last version of fetched data)
+ * --------- saved (mixed, timestamp|uui|identifier) change or set it to save current bind data back to api
+ * ----- profile
+ * ------ userid (integer)
+ * ------ name (string)
+ * ------ lastname (string)
+ * ------ email (string)
+ * ----- history (old and new values are stored here)
+ * ----- notification (realtime notification to user)
+ *
+ */
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+'use strict';
+
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const request = require('request-promise');
+const uuidV1 = require('uuid/v1');
+
+admin.initializeApp(functions.config().firebase);
+
+/**
+ * Emit event "fetchObject"
+ *
+ */
+exports.fetchObject = functions.database.ref('user/{userid}/storage/{object}/{objectid}/version').onWrite(event => {
+    const date = new Date();
+
+admin.database().ref('_events/' + uuidV1()).set({
+    'date': date.getTime(),
+    'object': event.params.object,
+    'objectid': event.params.objectid,
+    'userid': event.params.userid,
+    'function': 'fetchObject'
+});
+
+});
+
+/**
+ * Emmit event "saveObject"
+ *
+ */
+exports.saveObject = functions.database.ref('user/{userid}/storage/{object}/{objectid}/saved').onWrite(event => {
+
+    if (event.data.val()) {
+    admin.database().ref('user/' + event.params.userid + '/storage/' + event.params.object + '/' + event.params.objectid + '/data').once('value', function (data) {
+        const date = new Date();
+        admin.database().ref('_events/' + uuidV1()).set({
+            'date': date.getTime(),
+            'object': event.params.object,
+            'objectid': event.params.objectid,
+            'userid': event.params.userid,
+            'function': 'saveObject'
+        });
+
+    });
+}
+
+});
+
